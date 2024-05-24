cad9ea56a0b78bfabd00af9edce4c140fa1921e5
Annika Nichols <annika.nichols@uqconnect.edu.au>
2024-05-24 11:07:50 +0200

    Adding eye measurements

---
+++ cichlidanalysis/analysis/run_eye_vs_loadings.py
@@ -0,0 +1,99 @@
+import os
+from tkinter import *
+from tkinter.filedialog import askdirectory
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from py_tps import TPSFile
+
+
+def read_tps(file_path):
+    specimens = []
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        specimen = {}
+        landmarks = []
+        for line in lines:
+            line = line.strip()
+            if line.startswith("LM="):
+                if specimen:
+                    specimen['landmarks'] = landmarks
+                    specimens.append(specimen)
+                specimen = {'landmarks': []}
+                landmarks = []
+                specimen['num_landmarks'] = int(line.split('=')[1])
+            elif line.startswith("ID="):
+                specimen['id'] = line.split('=')[1]
+            else:
+                # Parse landmark coordinates
+                coords = list(map(float, line.split()))
+                landmarks.append(coords)
+        if specimen:
+            specimen['landmarks'] = landmarks
+            specimens.append(specimen)
+    return specimens
+
+
+if __name__ == '__main__':
+    # Allows user to select top directory and load all als files here
+    root = Tk()
+    rootdir = askdirectory(parent=root)
+    root.destroy()
+
+    # load data
+    loadings = pd.read_csv(os.path.join(rootdir, "pca_loadings.csv"), sep=',')
+    table_1 = pd.read_csv(os.path.join(rootdir, "table_1.csv"), sep=',')
+    diel_guilds = pd.read_csv(os.path.join(rootdir, "diel_guilds.csv"), sep=',')
+    voucher_key = pd.read_csv(os.path.join(rootdir, "01_specimen_voucher_key.csv"), sep=',')
+
+    body_shape_tps =TPSFile.read_file(os.path.join(rootdir, "06_landmark_data_body_shape.tps"))
+
+    tps_data = read_tps(os.path.join(rootdir, "06_landmark_data_body_shape.tps"))
+
+    species = 'Neotoa'
+    for specimen in tps_data:
+        print("Specimen ID:", specimen['id'])
+        print("Number of Landmarks:", specimen['num_landmarks'])
+        print("Landmarks:", specimen['landmarks'])
+
+    empty_array = np.empty((tps_data.__len__(), 2))
+
+    # get the species for each specimen/entry of the tps file
+    for specimen_n, specimen in enumerate(tps_data):
+        if specimen_n == 0:
+            specimen_species = pd.DataFrame({'species': [specimen['id'].split('_')[1]]})
+        else:
+            specimen_species_i = pd.DataFrame({'species': [specimen['id'].split('_')[1]]})
+            specimen_species = pd.concat([specimen_species, specimen_species_i], ignore_index=True)
+
+
+    for species_n, species in loadings.species:
+        specimens_for_species  = specimen_species.loc[specimen_species.species == species]
+
+        first = True
+        for specimen_n in specimens_for_species.index:
+            specimen_data = tps_data[specimen_n]
+            eye_west = specimen_data['landmarks'][17]
+            eye_east = specimen_data['landmarks'][19]
+            eye_size_h = math.sqrt((eye_west[0] - eye_east[0]) ** 2 + (eye_west[1] - eye_east[1]) ** 2)
+
+            eye_north = specimen_data['landmarks'][18]
+            eye_south = specimen_data['landmarks'][20]
+            eye_size_v = math.sqrt((eye_north[0] - eye_south[0]) ** 2 + (eye_north[1] - eye_south[1]) ** 2)
+
+            nose = specimen_data['landmarks'][1]
+            tail = specimen_data['landmarks'][7]
+            standard_len = math.sqrt((nose[0] - tail[0]) ** 2 + (nose[1] - tail[1]) ** 2)
+
+            if first:
+                species_measures = pd.DataFrame({'species': [specimen['id'].split('_')[1]], 'ID': specimen_data['id'],
+                                                 'eye_size_h': eye_size_h, 'eye_size_v': eye_size_v, 'standard_len':
+                                                     standard_len})
+                first = False
+            else:
+                species_measures_i = pd.DataFrame({'species': [specimen['id'].split('_')[1]], 'ID': specimen_data['id'],
+                                                 'eye_size_h': eye_size_h, 'eye_size_v': eye_size_v, 'standard_len':
+                                                     standard_len})
+                species_measures = pd.concat([species_measures, species_measures_i], ignore_index=True)
