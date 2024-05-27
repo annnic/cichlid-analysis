e0cafc02f32509a02035fb1499f47fe5f5676b36
Annika Nichols <annika.nichols@uqconnect.edu.au>
2024-05-27 14:16:02 +0200

    Eye sizes corr

--- cichlidanalysis/analysis/run_eye_vs_loadings.py
+++ cichlidanalysis/analysis/run_eye_vs_loadings.py
@@ -6,7 +6,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from py_tps import TPSFile
+from scipy.spatial import ConvexHull
 
 from cichlidanalysis.analysis.linear_regression import run_linear_reg, plt_lin_reg
 
@@ -74,27 +74,60 @@ if __name__ == '__main__':
             first = True
             for specimen_n in specimens_for_species.index:
                 specimen_data = tps_data[specimen_n]
-                eye_west = specimen_data['landmarks'][17]
-                eye_east = specimen_data['landmarks'][19]
+                eye_west = specimen_data['landmarks'][16]
+                eye_east = specimen_data['landmarks'][18]
                 eye_size_h = math.sqrt((eye_west[0] - eye_east[0]) ** 2 + (eye_west[1] - eye_east[1]) ** 2)
 
-                eye_north = specimen_data['landmarks'][18]
-                eye_south = specimen_data['landmarks'][20]
+                eye_north = specimen_data['landmarks'][17]
+                eye_south = specimen_data['landmarks'][19]
                 eye_size_v = math.sqrt((eye_north[0] - eye_south[0]) ** 2 + (eye_north[1] - eye_south[1]) ** 2)
 
-                nose = specimen_data['landmarks'][1]
-                tail = specimen_data['landmarks'][7]
+                nose = specimen_data['landmarks'][0]
+                tail = specimen_data['landmarks'][6]
                 standard_len = math.sqrt((nose[0] - tail[0]) ** 2 + (nose[1] - tail[1]) ** 2)
+
+                # get body outline (1, 3, 4, 7, 8, 11)
+                outline = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15])
+                points = np.ones((len(outline), 2))
+                points[:] = np.nan
+                for i, element in enumerate(outline):
+                    points[i, :] = specimen_data['landmarks'][element-1]
+                # Compute the convex hull of body
+                hull = ConvexHull(points)
+                body_area = hull.area
+
+                ## plotting the points and hull
+                # input_points = np.array(specimen_data['landmarks'][0:-1])
+                # fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 3))
+                # for ax in (ax1, ax2):
+                #     ax.plot(input_points[:, 0], input_points[:, 1], '.', color='k')
+                #     if ax == ax1:
+                #         ax.set_title('Given points')
+                #     else:
+                #         ax.set_title('Convex hull')
+                #         for simplex in hull.simplices:
+                #             ax.plot(points[simplex, 0], points[simplex, 1], 'c')
+                #         ax.plot(points[hull.vertices, 0], points[hull.vertices, 1], 'o', mec='r', color='none', lw=1,
+                #                 markersize=10)
+                #
+                #         # for point in input_points[16:20, :]:
+                #         ax.plot(input_points[16:20, 0], input_points[16:20, 1], 'o', mec='g', color='none', lw=1,
+                #                 markersize=10)
+                #     ax.set_xticks(range(10))
+                #
+                #     ax.set_yticks(range(10))
+                # # plt.savefig(os.path.join(rootdir, "convex_hull_2.png"))
+                # plt.close('all')
 
                 if first:
                     species_measures = pd.DataFrame({'species': [specimen_data['id'].split('_')[1]], 'ID': specimen_data['id'],
                                                      'eye_size_h': eye_size_h, 'eye_size_v': eye_size_v, 'standard_len':
-                                                         standard_len})
+                                                         standard_len, 'body_area': body_area})
                     first = False
                 else:
                     species_measures_i = pd.DataFrame({'species': [specimen_data['id'].split('_')[1]], 'ID': specimen_data['id'],
                                                      'eye_size_h': eye_size_h, 'eye_size_v': eye_size_v, 'standard_len':
-                                                         standard_len})
+                                                         standard_len, 'body_area': body_area})
                     species_measures = pd.concat([species_measures, species_measures_i], ignore_index=True)
 
             if first_species:
@@ -102,14 +135,23 @@ if __name__ == '__main__':
                 first_species = False
             else:
                 all_species_measures = pd.concat([all_species_measures, species_measures], ignore_index=True)
+        else:
+            print('Not including {}'.format(species))
 
     all_species_measures['eye_v_by_sl'] = all_species_measures.loc[:, 'eye_size_v'] / all_species_measures.loc[:,'standard_len']
     all_species_measures['eye_h_by_sl'] = all_species_measures.loc[:, 'eye_size_h'] / all_species_measures.loc[:,'standard_len']
+    all_species_measures['eye_h_by_ba'] = all_species_measures.loc[:, 'eye_size_h'] / all_species_measures.loc[:,'body_area']
 
     all_species_measures_mean = all_species_measures.groupby('species').mean().reset_index()
+    all_species_measures_mean.to_csv(os.path.join(rootdir, 'body_measurements.csv'), sep=',', index=False,
+                                     encoding='utf-8')
 
     combined_eye_loadings = pd.merge(loadings, all_species_measures_mean, on='species')
     combined_eye_loadings = combined_eye_loadings.set_index('species')
+
+    combined_eye_loadings.reset_index().to_csv(os.path.join(rootdir, 'body_measurements.csv'), sep=',', index=False,
+                                     encoding='utf-8')
+
     # pc1 vs day_night_dif
     model, r_sq = run_linear_reg(combined_eye_loadings.pc1, combined_eye_loadings.eye_v_by_sl)
     plt_lin_reg(rootdir, combined_eye_loadings.pc1, combined_eye_loadings.eye_v_by_sl, model, r_sq,
@@ -122,3 +164,12 @@ if __name__ == '__main__':
     model, r_sq = run_linear_reg(combined_eye_loadings.eye_v_by_sl, combined_eye_loadings.eye_h_by_sl)
     plt_lin_reg(rootdir, combined_eye_loadings.eye_v_by_sl, combined_eye_loadings.eye_h_by_sl, model, r_sq,
                 name_x='relative eye size v', name_y='relative eye size h', labels=True, figsize=(4, 4))
+
+    model, r_sq = run_linear_reg(combined_eye_loadings.pc1, combined_eye_loadings.eye_h_by_ba)
+    plt_lin_reg(rootdir, combined_eye_loadings.pc1, combined_eye_loadings.eye_h_by_ba, model, r_sq,
+                name_x='pc1 loading', name_y='relative eye size ba', labels=True, figsize=(4, 4))
+
+    model, r_sq = run_linear_reg(combined_eye_loadings.standard_len, combined_eye_loadings.body_area)
+    plt_lin_reg(rootdir, combined_eye_loadings.standard_len, combined_eye_loadings.body_area, model, r_sq,
+                name_x='standard_len', name_y='body_area', labels=True, figsize=(4, 4))
+    plt.close('all')
